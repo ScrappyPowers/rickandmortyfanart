cd8e6a9f9ada680c80a234feeff2be32d0cb8cdb
scrappypowers <misterball85@googlemail.com>
2017-08-19 12:52:24 +0100

    Make fan art index the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'fan_arts#index'
   resources :fan_arts
 end
