a01be1bc7a0de573e36b25c01630282682b5f077
scrappypowers <misterball85@googlemail.com>
2017-08-19 13:04:03 +0100

    Add new fan art page for creating fan art

--- app/controllers/fan_arts_controller.rb
+++ app/controllers/fan_arts_controller.rb
@@ -1,7 +1,6 @@
 class FanArtsController < ApplicationController
   def index
     @fan_arts = FanArt.all
-
   end
 
   def show
@@ -10,5 +9,21 @@ class FanArtsController < ApplicationController
 
   def new
     @fan_art = FanArt.new
+  end
+
+  def create
+    @fan_art = FanArt.new(fan_art_params)
+
+    if @fan_art.save
+      redirect_to @fan_art
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def fan_art_params
+    params.require(:fan_art).permit(:title, :author, :description)
   end
 end
